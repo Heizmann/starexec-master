2975aef1ae16b9b3b152a3cf1de8ead0da6e9030
Akihisa Yamada <ayamada@Akihisas-MacBook-Pro.local>
2020-04-21 17:10:32 +0900

    proper Content-Type

--- show.php
+++ show.php
@@ -3,6 +3,10 @@ $contents = file_get_contents($_GET['url']);
 
 if( substr($contents, 0, 5) == '<?xml' ) {
     header('Content-Type: text/xml');
+} else if( substr($contents, 0, 6) == '<?html' ) {
+    header('Content-Type: text/html' );
+} else {
+    header('Content-Type: text' );
 }
 echo $contents;
 ?>
